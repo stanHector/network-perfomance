c7da2bb3715cf3c2e5587d0ae89e93a24f529100
Hector Stanley <63668294+stanHector@users.noreply.github.com>
2021-11-16 19:58:57 +0100

    updated CORS

--- src/main/java/fhi0/DIDR/controller/InternetDowntimeController.java
+++ src/main/java/fhi0/DIDR/controller/InternetDowntimeController.java
@@ -19,7 +19,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://network-performance.netlify.app")
 @RestController
 @RequestMapping("/api/v1/")
 public class InternetDowntimeController {
